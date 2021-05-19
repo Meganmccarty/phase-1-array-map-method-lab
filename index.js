d4f9d2de955982b594be3d705a3c80bb0b89e82b
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-19 13:27:59 -0400

    commit tree cca6eab60188c9452680d87da4c2b95d73dc4eb7

--- index.js
+++ index.js
@@ -11,6 +11,49 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// used freeCodeCamp as a reference:
+// https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+
+/*const titleCased = (tutorials) = tutorials.map((title) => {
+
+    // split each word in a title into a separate array
+    const splitTitle = title.split(" ");
+
+    // create a new array with the split words
+    const wordsInTitle = splitTitle.map((word) => {
+        
+        // make the first letter of each word capital
+        const firstLetterCapital = word[0].toUpperCase() + word.substr(1);
+        return firstLetterCapital;
+    });
+
+    const newTitle = wordsInTitle.join(" ");
+    return newTitle;
+});
+
+console.log("New titles:", titleCased);
+console.log("Original array:", tutorials);*/
+
+function titleCased() {
+    const finalTitles = tutorials.map((title) => {
+
+        // split each word in a title into a separate array
+        const splitTitle = title.split(" ");
+    
+        // create a new array with the split words
+        const wordsInTitle = splitTitle.map((word) => {
+            
+            // make the first letter of each word capital
+            const firstLetterCapital = word[0].toUpperCase() + word.substr(1);
+            return firstLetterCapital;
+        });
+    
+        const newTitle = wordsInTitle.join(" ");
+        return newTitle;
+    });
+
+    return finalTitles;
 }
+
+console.log(titleCased());
+console.log(tutorials);
